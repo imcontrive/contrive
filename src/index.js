d9d0c42921ddfef9e150f257f73959c7ec831788
shubh <psubham94@gmail.com>
2019-05-12 15:39:48 +0530

    add react

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import store from './reducers/Store'
+import './index.css';
+import App from './App';
+import { Provider } from 'react-redux';
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>, document.getElementById('root'));
